c037cfecf7630a90304ccd3340f6bababea27360
Jim Moffitt <jmoffitt@twitter.com>
2020-07-13 14:58:59 -0600

    JM: building a refreshed Labs branch.

--- common/url_maker.rb
+++ common/url_maker.rb
@@ -1,66 +1,24 @@
 =begin
-Helper function for building URLs.
-
-Premium Search example:
-	labels = {}
-	labels['label'] = 'dev'
-	search-app.countURL = URLMaker.getSearchCountURL('premium', labels)
-
-Enterprise Search example:
-	labels = {}
-	labels['account_name'] = 'thinksnow'
-	labels['label'] = 'dev'
-	search-app.countURL = URLMaker.getSearchCountURL('enterprise', labels)
+Helper function for building URLs. Developed for Labs usage.
 =end
 
 class URLMaker
 
-	
-	#CONSTANTS
-	#root uris
-	PREMIUM_ROOT = "https://api.twitter.com/1.1/tweets/search/"
-	ENTERPRISE_ROOT = "https://gnip-api.twitter.com/search/"
-		
-	attr_accessor :product, #'premium' or 'enterprise'.
-	              :archive, #'30day' or 'fullarchive'.
-	              :account_name, #Enterprise only.
-	              :environment, #'dev', 'prod', and 'sandbox' are common examples.
-	              :url
+		attr_accessor :url, :search_tier, :search_period
 
 	def initialize
-		@search_type = 'premium'
-		@archive = '30day'
-		@account_name = nil   #Being explicit about this. If not set, we have 'premium'.
-		@environment = 'dev'  #Best practice is to have a 'dev' environment. So a decent default.
+		@data_url = "https://api.twitter.com/labs/2/tweets/search/recent" #Versioning.
+		@search_tier = 'labs'
+		@search_period = 'recent'
 	end
 
-	def getDataURL(product, archive, labels)
-		if product == 'premium'
-			#https://api.twitter.com/1.1/tweets/search/{archive}/{environment}.json
-			@url = "#{PREMIUM_ROOT}#{archive}/#{labels[:environment]}.json"
-		end
-
-		if product == 'enterprise'
-			#https://gnip-api.twitter.com/search/{archive}/accounts/{account_name}/{environment}.json
-			@url = "#{ENTERPRISE_ROOT}#{archive}/accounts/#{labels[:account_name]}/#{labels[:environment]}.json"
-		end
-		
-		@url
+	def get_data_url()
+		@data_url
 	end
 	
-	def getCountURL(product, archive, labels)
+	def get_count_url()
 
-		if product == 'premium'
-			#https://api.twitter.com/1.1/tweets/search/{archive}/{environment}/counts.json
-			@url = "#{PREMIUM_ROOT}#{archive}/#{labels[:environment]}/counts.json"
-		end
-
-		if product == 'enterprise'
-			#https://gnip-api.twitter.com/search/{archive}/accounts/{account_name}/{environment}/counts.json
-			@url = "#{ENTERPRISE_ROOT}#{archive}/accounts/#{labels[:account_name]}/#{labels[:environment]}/counts.json"
-		end
-		
-		@url
+		return "Counts endpoint not supported in Labs."
 		
 	end
 
